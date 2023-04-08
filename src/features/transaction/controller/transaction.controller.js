a4190b1d225ec5aec776c774a75f90bb8a6ea115
Ramlala <ramlala.yadav@bandhoo.com>
2023-04-08 21:09:51 +0530

    transation

--- src/features/transaction/controller/transaction.controller.js
+++ src/features/transaction/controller/transaction.controller.js
@@ -80,5 +80,56 @@ router.get("/:userId", async (req, res, next) => {
     next(exception);
   }
 });
+router.post("/reports", async (req, res, next) => {
+  try {
+    const { body: { userId, date } = {} } = req;
+    const user = await db.user.findByPk(userId);
+    const result = [];
+    if (user) {
+      let transactionList = await db.transaction.findAll();
+    
+      const roomIdList = transactionList.map((transaction) => {
+        return transaction.dataValues.roomId;
+      });
+
+      const roomList = await db.room.findAll({ where: { id: roomIdList } });
+      let idToRoomMap = {};
+      const hotelIdList = [];
+      roomList.forEach((room) => {
+        idToRoomMap[room.dataValues.id] = room.dataValues;
+        hotelIdList.push(room.dataValues.hotelId);
+      });
+      const hotelList = await db.hotel.findAll({ where: { id: hotelIdList } });
+      let idToHotelMap = {};
+      hotelList.forEach((hotel) => {
+        idToHotelMap[hotel.dataValues.id] = hotel.dataValues;
+      });
+
+      transactionList.forEach((transaction) => {
+        const entry = {};
+        const transactionObject = transaction.dataValues;
+        const roomId = transactionObject.roomId;
+        const room = idToRoomMap[roomId];
+        const hotel = idToHotelMap[room.hotelId];
+        entry.startDate = transactionObject.startDate;
+        entry.endDate = transactionObject.endDate;
+        const hotelDetails = {
+          name: hotel.name,
+          city: hotel.city,
+          url: hotel.url,
+          id: hotel.id,
+        };
+        const roomDetails = { beds: room.beds, id: room.id, price:room.price};
+        entry.hotelDetails = hotelDetails;
+        entry.roomDetails = roomDetails;
+        entry.id = transaction.id;
+        result.push(entry);
+      });
+    }
+    res.status(200).send({ result: result, total_count: result.length });
+  } catch (exception) {
+    next(exception);
+  }
+});
 
 module.exports = router;
